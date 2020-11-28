41ff76b2fe56e4bfe0f6d34fdb905b9abdd0f171
Nicolas <nicolas.nunez2@mail.udp.cl>
2020-11-27 22:29:14 -0300

    Algo

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import axios from 'axios';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
+/* Usando fetch */
+const Fetching = async () => {
+  const ret = await fetch('http://localhost:25000/perfiles');
+  const json = await ret.json();
+  console.log(json);
+}
+
+/* Usando Axios */
+const RequestToMyApi = () => {
+  return axios({
+    url: 'http://localhost:25000/perfiles',
+    method: 'get'
+  }).then(response => {
+    return response.data;
+  })
+};
+
 const Tab1: React.FC = () => {
-  return (
+
+  const [players, setPlayers] = React.useState([]);
+  
+  React.useEffect(() => {
+    RequestToMyApi().then(data => setPlayers(data));
+  }, []);
+
+   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
